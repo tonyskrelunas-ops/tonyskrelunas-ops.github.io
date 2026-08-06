d6fba46435c94e8f4bba10e0047d619a04b5a0bb
Tony Skrelunas <tonyskrelunas@Tonys-MacBook-Air.local>
2026-08-06 11:14:55 -0700

    Wire live MailerLite capture (account 2560671, form dAD9gF); keep contact form as email

--- js/main.js
+++ js/main.js
@@ -47,3 +47,7 @@
     setTimeout(function(){ document.querySelectorAll(".reveal:not(.is-in)").forEach(function(el){el.classList.add("is-in");}); }, 1600);
   }
 })();
+
+/* MailerLite Universal */
+(function(w,d,e,u,f,l,n){w[f]=w[f]||function(){(w[f].q=w[f].q||[]).push(arguments);},l=d.createElement(e),l.async=1,l.src=u,n=d.getElementsByTagName(e)[0],n.parentNode.insertBefore(l,n);})(window,document,'script','https://assets.mailerlite.com/js/universal.js','ml');
+ml('account','2560671');
